6fc6de5f34c9ea6a288462bb6e71b91ecd451a88
softyak <rsat.memis@gmail.com>
2023-03-26 21:44:06 +0300

    feat: some redirection process for selecting prompt

--- src/components/PromptListItem/index.jsx
+++ src/components/PromptListItem/index.jsx
@@ -3,22 +3,27 @@ import {
   Card, CardHeader, CardBody, CardFooter, Heading, Text, Button,
 } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { PromptItem } from '../../lib/types';
 import selectors from '../../store/selectors';
 import { selectPrompt } from '../../store/slices/chat';
+import { getPromptHashForURL } from '../../utils';
 
 function PromptListItem({ promptItem }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const selectedPrompt = useSelector(selectors.getSelectedPrompt);
+
   const { act } = selectedPrompt;
   const isSelected = act === promptItem.act;
 
   const handleSelectPrompt = () => {
     dispatch(selectPrompt(promptItem));
+    navigate('/');
   };
 
   return (
-    <Card>
+    <Card id={getPromptHashForURL(promptItem.act)}>
       <CardHeader>
         <Heading size="md">{promptItem.act}</Heading>
       </CardHeader>
---
+++ src/utils/index.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const getPromptHashForURL = (act) => act.toLowerCase().split(' ').join('-');
