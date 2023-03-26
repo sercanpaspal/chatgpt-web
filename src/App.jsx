7cb877617ed2b3c50eb99e92fd8f551f7c37c9c7
softyak <rsat.memis@gmail.com>
2023-03-26 22:17:09 +0300

    feat: an basic error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorFallback';
 import Pages from './pages';
 
 function App() {
   return (
-    <Pages />
+    // maybe we can use the withErrorBoundary hoc if we would decide to implement custom errors for pages
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Pages />
+    </ErrorBoundary>
   );
 }
 
